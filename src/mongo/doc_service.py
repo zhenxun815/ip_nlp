380f3f1e08fa86b5cb7bedf66d60139113c076bb
zhenxun815 <guoyiheng89@gmail.com>
2019-07-18 18:48:34 +0800

    move classification related options to clf_service

--- src/mongo/doc_service.py
+++ src/mongo/doc_service.py
@@ -10,18 +10,7 @@
 import time
 
 from pymongo import ASCENDING
-from pymongo import MongoClient
-
-
-def get_db(db_name):
-    """
-    connect to mongo and get the param specified db obj
-    :param db_name: the db's name
-    :return:
-    """
-    # client = MongoClient(host='192.168.1.205', port=27017, username='tqhy', password='tqhy817')
-    client = MongoClient('mongodb://tqhy:tqhy817@192.168.1.205:27017/?authSource=admin')
-    return client.get_database(db_name)  # same with: client[db_name]
+from mongo.connect import get_collection
 
 
 def create_index(db_name, clc_name, field_name, sort=ASCENDING):
@@ -33,8 +22,8 @@ def create_index(db_name, clc_name, field_name, sort=ASCENDING):
     :param sort: default direction is asc
     :return:
     """
-    db = get_db(db_name)
-    clc = db.get_collection(clc_name)
+
+    clc = get_collection(db_name, clc_name)
     clc.create_index([(field_name, sort)], background=True)
 
 
@@ -45,8 +34,7 @@ def remove_redundant(db_name, clc_name):
     :param clc_name:
     :return:
     """
-    db = get_db(db_name)
-    clc = db.get_collection(clc_name)
+    clc = get_collection(db_name, clc_name)
 
     redundant_docs = clc.aggregate([
             {'$group': {
@@ -68,7 +56,21 @@ def remove_redundant(db_name, clc_name):
             clc.remove(obj_ids[i])
 
 
-def find_by_classification(db_name, clc_name, limit=300, **kwargs):
+def find_by_clf(db_name, clc_name, limit=300, **kwargs):
+    """
+    find docs by classification infos and return a generator, whose item is a Bson obj
+    :param db_name:
+    :param clc_name:
+    :param limit:
+    :param kwargs:
+    :return:
+    """
+    cursor = find_cursor_by_clf(db_name, clc_name, limit, **kwargs)
+    while cursor.alive:
+        yield cursor.next()
+
+
+def find_cursor_by_clf(db_name, clc_name, limit, **kwargs):
     """
     get docs obj by classification params
     :param db_name:
@@ -80,57 +82,24 @@ def find_by_classification(db_name, clc_name, limit=300, **kwargs):
                 subClass
     :return:
     """
-    print('start search clf {}'.format(kwargs))
-    db = get_db(db_name)
-    clc = db.get_collection(clc_name)
+    print('start search tasks {}'.format(kwargs))
+    clc = get_collection(db_name, clc_name)
 
     query_filter = {}
     if 'section' in kwargs:
-        query_filter['section'] = kwargs['section']
+        query_filter['section'] = str(kwargs['section']).upper()
     else:
         print('section must not be null')
-        return
+        return {}
 
     if 'mainClass' in kwargs:
-        query_filter['mainClass'] = kwargs['mainClass']
+        query_filter['mainClass'] = str(kwargs['mainClass']).upper()
     if 'subClass' in kwargs:
-        query_filter['subClass'] = kwargs['subClass']
+        query_filter['subClass'] = str(kwargs['subClass']).upper()
 
     cursor = clc.find(query_filter).limit(limit)
-    print('search clf {} complete'.format(kwargs))
-    return list(cursor)
-
-
-def count_by_classification(db_name: str, clc_name: str, **kwargs):
-    """
-    get doc count of specified classification
-    :param db_name:
-    :param clc_name:
-    :param kwargs:
-                section required
-                mainClass
-                subClass
-    :return: int, if query section is null, return 0
-    """
-    print('start count clf {}'.format(kwargs))
-    db = get_db(db_name)
-    clc = db.get_collection(clc_name)
-
-    query_filter = {}
-    if 'section' in kwargs:
-        query_filter['section'] = kwargs['section']
-    else:
-        print('section must not be null')
-        return 0
-
-    if 'mainClass' in kwargs:
-        query_filter['mainClass'] = kwargs['mainClass']
-    if 'subClass' in kwargs:
-        query_filter['subClass'] = kwargs['subClass']
-
-    count = clc.count_documents(query_filter)
-    print('count is {}'.format(count))
-    return count
+    print('search tasks {} complete'.format(kwargs))
+    return cursor
 
 
 if __name__ == '__main__':
@@ -138,11 +107,11 @@ if __name__ == '__main__':
     clc_raw = 'raw'
     # remove_redundant('ip_doc', 'raw')
     start_time = time.time()
-    docs = find_by_classification(db_ip_doc, clc_raw, section='A', mainClass='01', subClass='B')
-    count = len(docs)
+    docs = find_by_clf(db_ip_doc, clc_raw, section='c', mainClass='08', subClass='b')
+    # count = len(list(docs))
+    # print('count is {}'.format(count))
     for doc in docs:
         print('find doc pubId {}'.format(doc['pubId']))
-    print('count is {}'.format(count))
 
     end_time = time.time()
     print('complete...,take time {}s'.format(end_time - start_time))
