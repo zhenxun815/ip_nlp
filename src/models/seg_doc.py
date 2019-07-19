82b83682a749c371b5c97a8d2c0b76b514be5b56
zhenxun815 <guoyiheng89@gmail.com>
2019-07-19 19:34:06 +0800

    commmit

---
+++ src/models/seg_doc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Description: 
+# @File: seg_doc.py
+# @Project: ip_nlp
+# @Author: Yiheng
+# @Email: GuoYiheng89@gmail.com
+# @Time: 7/19/2019 9:55
