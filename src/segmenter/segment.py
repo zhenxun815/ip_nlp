0c592c2c16837522d8f42c36bb7e7576d8a80fd0
zhenxun815 <guoyiheng89@gmail.com>
2019-08-02 19:11:11 +0800

    add logger

--- src/segmenter/segment.py
+++ src/segmenter/segment.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
-import re
-
-import jieba
-import jieba.analyse
 
 # @Description:
 # @File: segment.py
@@ -12,8 +7,18 @@ import jieba.analyse
 # @Author: Yiheng
 # @Email: GuoYiheng89@gmail.com
 # @Time: 7/17/2019 10:58
+import os
+import re
 
-jieba.load_userdict('../../resources/cnki_dict.txt')
+import jieba
+import jieba.analyse
+
+from common import logger_factory
+from common import path_config
+
+logger = logger_factory.get_logger('segment')
+
+jieba.load_userdict(path_config.cnki_dict)
 
 # match decimal or single character
 digit_pattern = re.compile(r'^[0-9]+(\.[0-9]+)?[a-zA-Z%‰]?')
@@ -37,7 +42,7 @@ def is_digit(words: str):
 
 
 def is_chemistry(words: str):
-    print('str to judge is {}'.format(words))
+    logger.info(f'str to judge is {words}')
     matcher1 = chemistry_pattern1.match(words)
     matcher2 = chemistry_pattern2.match(words)
     matcher = matcher1 if matcher1 else matcher2
@@ -83,7 +88,7 @@ def test_jieba():
 
 
 if __name__ == '__main__':
-    text = '本发明H2O涉及S2园林机电Al1.0%技术领域\t实用新型 公开，具体的1000ppm说是一种盆\n栽土壤0.25%养护      ' \
+    text = '本发明H2O涉及S2园林机电Al1.0%技术领域\t实用新型 公开，具体云计算的1000ppm说是一种盆\n栽土壤0.25%养护      ' \
            '作业平台，包括200B1机架、平连接，水管(53)另一端通过水泵H289%与水箱(51)相连接；所述)的气管0.01相连接。'
     seg_list_accuracy = jieba.cut(text, cut_all=False)
     print(' '.join(seg_list_accuracy))
