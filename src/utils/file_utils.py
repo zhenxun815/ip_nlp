e9e61aecb9d8d2694d22dc57afdabd3838e36fb4
zhenxun815 <guoyiheng89@gmail.com>
2019-09-17 16:10:58 +0800

    update

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -18,22 +18,33 @@ def make_dirs(base_dir, sub_dir):
     return _dir
 
 
-def save_list2file(list2write, dest_file: str, work=None):
+def save_dict2file(dict2save: dict, dest_file: str, work=None, split=':'):
+    print(f'start writing data to: {dest_file}')
+    with open(dest_file, 'a', encoding='utf-8') as f:
+        for k, v in dict2save.items():
+            print(f'item to write k {k},v {v}')
+            if work:
+                k, v = work(k, v)
+            f.write(f'{k}{split}{v}\n')
+        f.flush()
+    print(f'complete writing data to: {dest_file}')
+
+
+def save_list2file(list2save, dest_file: str, work=None):
     """
     write list elements to file line by line
     :param work: func, process each item in the list
-    :param list2write:
+    :param list2save:
     :param dest_file
     :return:
     """
     print(f'start writing data to: {dest_file}')
     with open(dest_file, 'a', encoding='utf-8') as f:
-        for item in list2write:
-            # print('item to write {}'.format(item))
+        for item in list2save:
+            # print(f'item to write {item}')
             if work:
                 item = work(item)
             f.write(str(item) + '\n')
-            # f.flush()
     print(f'complete writing data to: {dest_file}')
 
 
@@ -47,6 +58,7 @@ def get_files(dir_path, name_regx=None):
     """
     if os.path.isdir(dir_path):
         files = os.listdir(dir_path)
+
         if name_regx:
             _pattern = re.compile(name_regx)
             return [os.path.join(dir_path, file) for file in files if _pattern.match(file)]
@@ -59,36 +71,32 @@ def read_line(file2read, work, split=None):
     print(f'start reading {file2read}')
     with open(file2read, encoding='utf-8') as f:
         for line in f:
+            content = line.strip()
             if split:
-                work_content = line.strip().split(split)
-                if len(work_content) > 1:
-                    yield work(work_content)
+                content = content.split(split)
+            if work:
+                yield work(content)
             else:
-                yield work(line.strip())
+                yield content
     print(f'complete reading {file2read}')
 
 
-def remove_redundant(origin_file, dest_file):
+def remove_redundant(origin_file, dest_file, keep_order=True):
     """
     make lines unique and write to a new file
+    :param keep_order: whether to keep the order of origin list or not
     :param origin_file:
     :param dest_file:
     :return:
     """
     print(f'origin file is: {origin_file}, dest file is: {dest_file}')
     lines = read_line(origin_file, lambda line: line)
-
-    unique_lines = []
-    for line in lines:
-        if unique_lines.count(line) > 0:
-            print('line redundant...')
-            continue
-        print('add line ...')
-        unique_lines.append(line)
-
-    save_list2file(unique_lines, dest_file)
+    if keep_order:
+        save_list2file(list(dict.fromkeys(lines)), dest_file)
+    else:
+        save_list2file(list(set(lines)), dest_file)
 
 
 if __name__ == '__main__':
 
-    remove_redundant('E:/ip_data/train/1300/test.txt', 'E:/ip_data/train/1300/test2.txt')
+    remove_redundant('E:/cnki_trans_clean4.txt', 'E:/cnki_trans_clean5.txt')
